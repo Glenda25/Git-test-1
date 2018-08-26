a71a341f247a4e2b939f5a4dc835892057eef18a
Glenda Mullan <Glenda@glendas-imac-2.local>
2018-08-26 14:14:31 +1000

    Initial Commit

---
+++ Git test 1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Git test 1
+//
+//  Created by Glenda Mullan on 26/8/18.
+//  Copyright © 2018 Glenda Mullan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
